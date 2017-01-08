1909d835a7c8229f9764d86c3e8ec0fcc19e37ce
cc <ccking@foxmail.com>
2017-01-09 02:25:52 +0800

    update docs of badge

--- example/views/Badge.vue
+++ example/views/Badge.vue
@@ -3,10 +3,15 @@
     <h1>徽标 Badge</h1>
     
     <h2>例子</h2>
+    
+    <p>
+      所有徽标默认显示在右边
+    </p>
 
     <h3>放在图标上</h3>
-    <r-icon class="badge-icon" large v-badge:star.inner.icon>face</r-icon>
-    <r-icon class="badge-icon green-after" large v-badge:navigation.inner.icon.left>camera</r-icon>
+    <r-icon class="badge-icons red-after" large v-badge:10.inner>call</r-icon>
+    <r-icon class="badge-icons" large v-badge:star.inner.icon>face</r-icon>
+    <r-icon class="badge-icons green-after" large v-badge:navigation.inner.icon.left>camera</r-icon>
 
     <h3>放在普通元素上</h3>
     <p>数字</p>
@@ -22,8 +27,52 @@
     <span class="badge-link orange-after" v-badge:dot.left>This is text</span>
     
     <h2>API</h2>
+    <h3>参数</h3>
+    <p>
+      添加了 v-badge 指令的元素可以添加颜色 class: '[color]-after' (color 为色值) <br>
+      PS: 因为徽标存在于伪类 after 上，所以用 [color]-after 调整颜色    
+    </p>
+
+    <h3>指令说明</h3>
+    <table class="bordered responsive-table">
+      <thead>
+        <th>指令</th>
+        <th>说明</th>
+      </thead>
+      <tbody>
+        <tr>
+          <td>v-badge:star.inner.icon <br> 指令的参数 arg</td>
+          <td>
+            指令的参数 arg (即 star) 对应 徽标显示的内容<br>
+            如果徽标是 icon，则指令的 arg 为 icon-font 对应的值
+          </td>
+        </tr>
+        <tr>
+          <td>v-badge:navigation.inner.icon.left <br> 指令的 modifiers   </td>
+          <td>
+            inner: 徽标显示在元素内 <br>
+            icon: 徽标是否为 icon <br>  
+            left: 徽标显示位置
+          </td>
+        </tr>
+        <tr>
+          <td>v-badge:55.left <br> 指令的参数 arg </td>
+          <td>
+            此指令的 arg 为 55，徽标显示为纯数字 55
+          </td>
+        </tr>
+        <tr>
+          <td>v-badge:dot <br> 指令的参数 arg </td>
+          <td>
+            此指令的 arg 为 dot，徽标显示为一个点 (可做提示用)
+          </td>
+        </tr>
+        
+      </tbody> 
+    </table>
     <code>
-      &lt;r-icon class="badge-icon" large v-badge:star.inner.icon&gt;face&lt;/r-icon&gt;
+      &lt;r-icon class="red-after" large v-badge:10.inner&gt;call&lt;/r-icon&gt;
+      &lt;r-icon large v-badge:star.inner.icon&gt;face&lt;/r-icon&gt;
       &lt;span class="badge-demo" v-badge:star.icon.left&gt;&lt;/span&gt;
       &lt;a class="badge-link red-after" v-badge:dot&gt;This is a link&lt;/a&gt;
     </code>
@@ -40,7 +89,7 @@ export default {
   h3 {
     margin-bottom 1rem
   }
-  .badge-icon {
+  .badge-icons {
     display inline-block
     margin 0 20px
     text-align center
--- example/views/Tooltip.vue
+++ example/views/Tooltip.vue
@@ -1,7 +1,9 @@
 <template>
   <article>
     <h1>提示框 Tooltip</h1>
-    
+    <p>
+      tooltip 用法类似元素的 title 属性，所以做成了指令而不是组件
+    </p>
     <h2>例子</h2>
     
     <h3>四向</h3>
@@ -17,9 +19,27 @@
     
     
     <h2>API</h2>
+
+    <h3>指令说明</h3>
+    <table class="bordered responsive-table">
+      <thead>
+        <th>指令</th>
+        <th>说明</th>
+      </thead>
+      <tbody>
+        <tr>
+          <td>v-tooltip:top="{ text: '提示在上边' }"</td>
+          <td>指令的参数 arg (即 top) 对应 tooltip 的显示位置</td>
+        </tr>
+        <tr>
+          <td>v-tooltip:top="{ text: '提示在上边' }"</td>
+          <td>指令的 value 对象的 text 为 tooltip 的展示内容</td>
+        </tr>
+      </tbody> 
+    </table>
     <code>
       &lt;span class="green white-text" v-tooltip:top="{ text: '提示在上边' }"&gt;上&lt;/span&gt;
-      &lt;r-btn class="grey white-text" v-tooltip:right="{ text: '来自按钮的提示文字' }"&gt;我是按钮&lt;/r-btn&gt;
+      &lt;r-btn class="grey white-text" v-tooltip:right="{ text: '我真的是个按钮' }"&gt;我是按钮&lt;/r-btn&gt;
     </code>
   </article>
 </template>
