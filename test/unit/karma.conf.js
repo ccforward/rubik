6f53b6bc703a28ca6f96dd1970fd9dc6fc3fc291
cc <ccking@foxmail.com>
2017-02-18 14:38:10 +0800

    add karma for unit test

---
+++ test/unit/karma.conf.js
@@ -0,0 +1,63 @@
+// Karma configuration
+
+const webpackConfig = require('../../build/webpack.base.conf');
+
+delete webpackConfig.entry;
+
+
+module.exports = function(config) {
+  config.set({
+    browsers: ['Chrome'],
+
+    frameworks: ['mocha', 'sinon-chai'],
+
+    // list of files / patterns to load in the browser
+    files: [
+      './index.js'
+    ],
+
+    preprocessors: {
+      './index.js': ['webpack', 'sourcemap']
+    },
+
+    webpack: webpackConfig,
+
+    plugins: [
+      'karma-webpack',
+      'karma-sourcemap-loader',
+      'karma-mocha',
+      'karma-chai',
+      'karma-sinon-chai',
+      'karma-chrome-launcher',
+      'karma-phantomjs-launcher'
+    ],
+
+    reporters: ['progress'],
+
+    // reporters: ['spec', 'coverage'],
+
+    coverageReporter: {
+      dir: './coverage',
+      reporters: [
+        { type: 'lcov', subdir: '.' },
+        { type: 'text-summary' }
+      ]
+    },
+
+    port: 9876,
+
+
+    colors: true,
+
+
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    concurrency: Infinity
+  })
+}
