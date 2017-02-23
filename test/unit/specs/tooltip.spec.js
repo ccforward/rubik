513d33ae8adf8dc786f124e87bc6ce00559776e5
cc <ccking@foxmail.com>
2017-02-23 21:21:42 +0800

    add tooltip unit test 🎅🏽  🐧  🐳

---
+++ test/unit/specs/tooltip.spec.js
@@ -0,0 +1,41 @@
+import { fireEvent, createVM, destroyVM } from '../vm'
+
+describe('Tooltip', () => {
+  const txt = 'top tooltip'
+  let vm
+  afterEach(() => {
+    destroyVM(vm)
+  })
+
+
+  it('create', done => {
+    vm = createVM({
+      template: `<div class="tt-top" v-tooltip:top="{ text: '${txt}' }">top</div>`
+    }, true)
+
+    vm.$nextTick( _ => {
+      const tt = document.querySelector('.tt-top')
+
+      expect(vm.$el.dataset.tooltip).to.be.equal(txt)
+      expect(getComputedStyle(tt, ':after').content.toString().replace(/"/g, '') == vm.$el.dataset.tooltip).to.be.true
+      done()
+    })
+  })
+
+  it('mouse show-hide', done => {
+    vm = createVM({
+      template: `<div class="tt-left" v-tooltip:left="{ text: '123' }">left</div>`
+    }, true)
+
+    vm.$nextTick( _ => {
+      const tt = document.querySelector('.tt-left')
+      expect(getComputedStyle(tt, ':after').visibility).to.be.equal('hidden')
+      fireEvent(vm.$el, 'mouseenter')
+      setTimeout( _ => {
+        expect(getComputedStyle(tt, ':after').visibility).to.not.equal('hidden')
+      }, 1000)
+      done()
+    })
+  })
+
+});
