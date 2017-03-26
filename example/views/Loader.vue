cf3105a06d5a1d834920f1c6a952c192d65061f1
cc <ccking@foxmail.com>
2017-03-26 19:15:13 +0800

    add loader component 🥇  ⚓️  🔫

--- example/views/Loader.vue
+++ example/views/Loader.vue
@@ -9,14 +9,10 @@
     <section class="loader-box">
       <r-btn info small @click.native="showLoader('normal')">Normal</r-btn>
       <r-loader normal :show="type.normal"></r-loader>
-    </section>
-
-    <section class="loader-box">
+   
       <r-btn info small @click.native="showLoader('half')">Half</r-btn>
       <r-loader half :show="type.half"></r-loader>
-    </section>
-
-    <section class="loader-box">
+   
       <r-btn info small @click.native="showLoader('reverse')">Reverse</r-btn>
       <r-loader reverse :show="type.reverse"></r-loader>
     </section>
@@ -25,14 +21,10 @@
     <section class="loader-box">
       <r-btn info small @click.native="showLoader('bar')">Normal</r-btn>
       <r-loader bar :show="type.bar"></r-loader>
-    </section>
-
-    <section class="loader-box">
+    
       <r-btn info small @click.native="showLoader('round')">Round</r-btn>
       <r-loader bar-round :show="type.round"></r-loader>
-    </section>
-
-    <section class="loader-box">
+    
       <r-btn info small @click.native="showLoader('barReverse')">reverse</r-btn>
       <r-loader bar-reverse :show="type.barReverse"></r-loader>
     </section>
@@ -56,33 +48,105 @@
       </thead>
       <tbody>
         <tr>
-          <td>medium</td>
-          <td>中等尺寸</td>
+          <td>show</td>
+          <td>是否显示</td>
+          <td>Boolean</td>
+          <td>false</td>
+        </tr>
+        <tr>
+          <td>normal</td>
+          <td>默认类型</td>
           <td>Boolean</td>
           <td>-</td>
         </tr>
         <tr>
-          <td>large</td>
-          <td>大尺寸</td>
+          <td>half</td>
+          <td>半圆形的默认类型</td>
           <td>Boolean</td>
           <td>-</td>
         </tr>
         <tr>
-          <td>xLarge</td>
-          <td>特大尺寸</td>
+          <td>reverse</td>
+          <td>反向旋转的默认类型</td>
           <td>Boolean</td>
           <td>-</td>
         </tr>
+        <tr>
+          <td>bar</td>
+          <td>loading条类型</td>
+          <td>Boolean</td>
+          <td>-</td>
+        </tr>
+        <tr>
+          <td>bar-round</td>
+          <td>圆角的loading条</td>
+          <td>Boolean</td>
+          <td>-</td>
+        </tr>
+        <tr>
+          <td>bar-reverse</td>
+          <td>反向动画的loading条</td>
+          <td>Boolean</td>
+          <td>-</td>
+        </tr>
+        <tr>
+          <td>double</td>
+          <td>两个默认的loading</td>
+          <td>Boolean</td>
+          <td>-</td>
+        </tr>
+
       </tbody> 
     </table>
     <Markup :lang="'html'">
-      &lt;r-icon&gt;call&lt;/r-icon&gt;
+      &lt;r-btn info small @click.native="showLoader('normal')"&gt;Normal&lt;/r-btn&gt;
+      &lt;r-loader normal :show="type.normal"&gt;&lt;/r-loader&gt;
 
-      &lt;r-icon medium&gt;call&lt;/r-icon&gt;
+      &lt;r-btn info small @click.native="showLoader('round')"&gt;Round&lt;/r-btn&gt;
+      &lt;r-loader bar-round :show="type.round"&gt;&lt;/r-loader&gt;
 
-      &lt;r-icon large&gt;call&lt;/r-icon&gt;
+      &lt;r-btn info small @click.native="showLoader('reverse')"&gt;Reverse&lt;/r-btn&gt;
+      &lt;r-loader reverse :show="type.reverse"&gt;&lt;/r-loader&gt;
 
-      &lt;r-icon xLarge&gt;call&lt;/r-icon&gt;
+      &lt;r-btn info small @click.native="showLoader('bar')"&gt;Normal&lt;/r-btn&gt;
+      &lt;r-loader bar :show="type.bar"&gt;&lt;/r-loader&gt;
+
+      &lt;r-btn info small @click.native="showLoader('round')"&gt;Round&lt;/r-btn&gt;
+      &lt;r-loader bar-round :show="type.round"&gt;&lt;/r-loader&gt;
+
+      &lt;r-btn info small @click.native="showLoader('barReverse')"&gt;reverse&lt;/r-btn&gt;
+      &lt;r-loader bar-reverse :show="type.barReverse"&gt;&lt;/r-loader&gt;
+
+      &lt;r-btn info small @click.native="showLoader('double')"&gt;Double&lt;/r-btn&gt;
+      &lt;r-loader double :show="type.double"&gt;&lt;/r-loader&gt;
+
+    </Markup>
+
+    <Markup :lang="'js'">
+      export default {
+        
+        data () {
+          return {
+            type: {
+              normal: false,
+              half: false,
+              reverse: false,
+              bar: false,
+              round: false,
+              barReverse: false,
+              double: false
+            }
+          }
+        },
+        methods: {
+          showLoader(loader){
+            this.type[loader] = true
+            setTimeout( _ => {
+              this.type[loader] = false
+            }, 1500)
+          }
+        }
+      }
     </Markup>
   </article>
 </template>
