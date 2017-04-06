81ae2057ee7e813740453ddb3671096f0d340b1a
cc <ccking@foxmail.com>
2017-04-06 16:07:03 +0800

    spliting code for examples 🎃  🏈 🌴

--- example/router/index.js
+++ example/router/index.js
@@ -1,34 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import About from '../views/About.vue'
-import Color from '../views/Color.vue'
-import Tag from '../views/Tag.vue'
-import Grid from '../views/Grid.vue'
-import Helper from '../views/Helper.vue'
-import Shadow from '../views/Shadow.vue'
-import Table from '../views/Table.vue'
-import Alert from '../views/Alert.vue'
-import Breadcrumb from '../views/Breadcrumb.vue'
-import Card from '../views/Card.vue'
-import Slider from '../views/Slider.vue'
-import Dropdown from '../views/Dropdown.vue'
-import Tab from '../views/Tab.vue'
-import Modal from '../views/Modal.vue'
-import Icon from '../views/Icon.vue'
-import Button from '../views/Button.vue'
-import Sidenav from '../views/Sidenav.vue'
-import Pagination from '../views/Pagination.vue'
-import Input from '../views/Input.vue'
-import Radio from '../views/Radio.vue'
-import Checkbox from '../views/Checkbox.vue'
-import Select from '../views/Select.vue'
-import File from '../views/File.vue'
-import Badge from '../views/Badge.vue'
-import Tooltip from '../views/Tooltip.vue'
-import Toast from '../views/Toast.vue'
-import Waves from '../views/Waves.vue'
-import Lazy from '../views/Lazy.vue'
-import Loader from '../views/Loader.vue'
 
 Vue.use(Router)
 
@@ -36,34 +8,258 @@ const router = new Router({
   routes:[
     { name: 'home', path: '/', component: About},
     { name: 'about', path: '/about', component: About},
-    { name: 'color', path: '/color', component: Color},
-    { name: 'grid', path: '/grid', component: Grid},
-    { name: 'helper', path: '/helper', component: Helper},
-    { name: 'alert', path: '/alert', component: Alert},
-    { name: 'shadow', path: '/shadow', component: Shadow},
-    { name: 'table', path: '/table', component: Table},
-    { name: 'breadcrumb', path: '/breadcrumb', component: Breadcrumb},
-    { name: 'card', path: '/card', component: Card},
-    { name: 'slider', path: '/slider', component: Slider},
-    { name: 'dropdown', path: '/dropdown', component: Dropdown},
-    { name: 'tab', path: '/tab', component: Tab},
-    { name: 'modal', path: '/modal', component: Modal},
-    { name: 'tag', path: '/tag', component: Tag},
-    { name: 'icon', path: '/icon', component: Icon},
-    { name: 'button', path: '/button', component: Button},
-    { name: 'sidenav', path: '/sidenav', component: Sidenav},
-    { name: 'pagination', path: '/pagination', component: Pagination},
-    { name: 'input', path: '/input', component: Input},
-    { name: 'radio', path: '/radio', component: Radio},
-    { name: 'checkbox', path: '/checkbox', component: Checkbox},
-    { name: 'select', path: '/select', component: Select},
-    { name: 'file', path: '/file', component: File},
-    { name: 'badge', path: '/badge', component: Badge},
-    { name: 'tooltip', path: '/tooltip', component: Tooltip},
-    { name: 'toast', path: '/toast', component: Toast},
-    { name: 'waves', path: '/waves', component: Waves},
-    { name: 'img-lazy', path: '/img-lazy', component: Lazy},
-    { name: 'loader', path: '/Loader', component: Loader}
+    {
+      name: 'color',
+      path: '/color',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Color.vue'));
+        })
+      }
+    },
+    {
+      name: 'grid',
+      path: '/grid',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Grid.vue'))
+        })
+      }
+    },
+    {
+      name: 'helper',
+      path: '/helper',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Helper.vue'))
+        })
+      }
+    },
+    {
+      name: 'alert',
+      path: '/alert',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Alert.vue'))
+        })
+      }
+    },
+    {
+      name: 'shadow',
+      path: '/shadow',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Shadow.vue'))
+        })
+      }
+    },
+    {
+      name: 'table',
+      path: '/table',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Table.vue'))
+        })
+      }
+    },
+    {
+      name: 'breadcrumb',
+      path: '/breadcrumb',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Breadcrumb.vue'))
+        })
+      }
+    },
+    {
+      name: 'card',
+      path: '/card',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Card.vue'))
+        })
+      }
+    },
+    {
+      name: 'slider',
+      path: '/slider',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Slider.vue'))
+        })
+      }
+    },
+    {
+      name: 'dropdown',
+      path: '/dropdown',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Dropdown.vue'))
+        })
+      }
+    },
+    {
+      name: 'tab',
+      path: '/tab',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Tab.vue'))
+        })
+      }
+    },
+    {
+      name: 'modal',
+      path: '/modal',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Modal.vue'))
+        })
+      }
+    },
+    {
+      name: 'tag',
+      path: '/tag',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Tag.vue'))
+        })
+      }
+    },
+    {
+      name: 'icon',
+      path: '/icon',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Icon.vue'))
+        })
+      }
+    },
+    {
+      name: 'button',
+      path: '/button',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Button.vue'))
+        })
+      }
+    },
+    {
+      name: 'sidenav',
+      path: '/sidenav',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Sidenav.vue'))
+        })
+      }
+    },
+    {
+      name: 'pagination',
+      path: '/pagination',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Pagination.vue'))
+        })
+      }
+    },
+    {
+      name: 'input',
+      path: '/input',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Input.vue'))
+        })
+      }
+    },
+    {
+      name: 'radio',
+      path: '/radio',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Radio.vue'))
+        })
+      }
+    },
+    {
+      name: 'checkbox',
+      path: '/checkbox',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Checkbox.vue'))
+        })
+      }
+    },
+    {
+      name: 'select',
+      path: '/select',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Select.vue'))
+        })
+      }
+    },
+    {
+      name: 'file',
+      path: '/file',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/File.vue'))
+        })
+      }
+    },
+    {
+      name: 'badge',
+      path: '/badge',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Badge.vue'))
+        })
+      }
+    },
+    {
+      name: 'tooltip',
+      path: '/tooltip',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Tooltip.vue'))
+        })
+      }
+    },
+    {
+      name: 'toast',
+      path: '/toast',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Color.vue'))
+        })
+      }
+    },
+    {
+      name: 'waves',
+      path: '/waves',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Waves.vue'))
+        })
+      }
+    },
+    {
+      name: 'img-lazy',
+      path: '/img-lazy',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Lazy.vue'))
+        })
+      }
+    },
+    {
+      name: 'loader',
+      path: '/Loader',
+      component: resolve => {
+        require.ensure([], require => {
+          resolve(require('../views/Loader.vue'))
+        })
+      }
+    }
   ]
 })
 
